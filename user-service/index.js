fa83b8c0110d657e6c035db5da609d80050013ed
Lau Siaw Sam <lausiawsam98@gmail.com>
2022-08-30 15:36:54 +0800

    Format user-service index file (prettier)

--- user-service/index.js
+++ user-service/index.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import cors from 'cors';
+import express from 'express'
+import cors from 'cors'
 
-const app = express();
+const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors()) // config cors so that front-end can use
 app.options('*', cors())
-import { createUser } from './controller/user-controller.js';
+import { createUser } from './controller/user-controller.js'
 
 const router = express.Router()
 
@@ -15,8 +15,8 @@ router.get('/', (_, res) => res.send('Hello World from user-service'))
 router.post('/', createUser)
 
 app.use('/api/user', router).all((_, res) => {
-    res.setHeader('content-type', 'application/json')
-    res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('content-type', 'application/json')
+  res.setHeader('Access-Control-Allow-Origin', '*')
 })
 
-app.listen(8000, () => console.log('user-service listening on port 8000'));
+app.listen(8000, () => console.log('user-service listening on port 8000'))
