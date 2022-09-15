1b16107ae8884766b16eeb9b62b809d326766e67
Tay Jun Yang <danny_tayjunyang@hotmail.com>
2022-09-15 12:36:36 +0800

    Modify Home page and create/refractor components

---
+++ frontend/src/components/FindMatch.js
@@ -0,0 +1,82 @@
+import {
+    Box,
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    FormControl,
+    InputLabel,
+    MenuItem,
+    Select,
+    Typography
+  } from '@mui/material'
+  import { useState } from 'react'
+  import { useNavigate } from 'react-router-dom'
+  
+  const SelectDifficulty = () => {
+    const [difficulty, setDifficulty] = useState('');
+    const [isDialogOpen, setIsDialogOpen] = useState(false)
+    const [dialogTitle, setDialogTitle] = useState('')
+    const [dialogMsg, setDialogMsg] = useState('')
+    const navigate = useNavigate()
+
+    const handleFindMatch = (difficulty) => {
+      if (difficulty === "Easy" || difficulty === "Medium" || difficulty === "Hard") {
+        console.log('Difficulty Level Selected: ' + difficulty)
+        navigate('/matching')
+      } else {
+        setErrorDialog('Please select a difficulty level.')
+      }
+    }
+
+    const closeDialog = () => setIsDialogOpen(false)
+
+    const setErrorDialog = (msg) => {
+      setIsDialogOpen(true)
+      setDialogTitle('Error')
+      setDialogMsg(msg)
+    }
+
+    return (
+      <Box sx={{ my: 3, mx: 2 }}>
+        <Typography sx={{ mb:3 }} variant={'h5'}>
+          {'Find Match'}
+        </Typography>
+
+        <FormControl fullWidth sx={{ mb:3 }}>
+          <InputLabel>Select Difficulty</InputLabel>
+          <Select
+            value={difficulty}
+            label="Select Difficulty"
+            onChange={(e) => setDifficulty(e.target.value)}
+          >
+            <MenuItem value="Easy">Easy</MenuItem>
+            <MenuItem value="Medium">Medium</MenuItem>
+            <MenuItem value="Hard">Hard</MenuItem>
+          </Select>
+        </FormControl>
+
+        <Button
+          variant={'outlined'}
+          onClick={() => handleFindMatch(difficulty)}
+        >
+          {'Find Match'}
+        </Button>
+
+        <Dialog open={isDialogOpen} onClose={closeDialog}>
+          <DialogTitle>{dialogTitle}</DialogTitle>
+          <DialogContent>
+            <DialogContentText>{dialogMsg}</DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={closeDialog}>OK</Button>
+          </DialogActions>
+        </Dialog>
+      </Box>
+    )
+  }
+
+  export default SelectDifficulty
+  
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,32 +1,8 @@
-import { Box, Button, Divider, Typography, FormControl, InputLabel, MenuItem, Select } from '@mui/material'
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import SelectDifficulty from '../components/SelectDifficulty'
+import { Box, Divider, Typography } from '@mui/material'
+import AccountManagement from '../components/AccountManagement'
+import FindMatch from '../components/FindMatch'
 
 const HomePage = () => {
-  const [isDialogOpen, setIsDialogOpen] = useState(false)
-  const [dialogTitle, setDialogTitle] = useState('')
-  const [dialogMsg, setDialogMsg] = useState('')
-  const [difficulty, setDifficulty] = useState('');
-  const navigate = useNavigate()
-
-  const handleFindMatch = (difficulty) => {
-    if (difficulty === "Easy" || difficulty === "Medium" || difficulty === "Hard") {
-      console.log('Difficulty Level Selected: ' + difficulty)
-      navigate('/matching')
-    } else {
-      setErrorDialog('Please select a difficulty level.')
-    }
-  }
-
-  const closeDialog = () => setIsDialogOpen(false)
-
-  const setErrorDialog = (msg) => {
-    setIsDialogOpen(true)
-    setDialogTitle('Error')
-    setDialogMsg(msg)
-  }
-
   return (
     <Box
       sx={{
@@ -41,49 +17,13 @@ const HomePage = () => {
     >
       <Box sx={{ my: 3, mx: 2 }}>
         <Typography variant={'h3'}>
-          {'Home Page'}
+          {'Home'}
         </Typography>
       </Box>
       <Divider variant="middle" />
-      <Box sx={{ my: 3, mx: 2 }}>
-        <Typography sx={{ marginBottom: '2rem' }} variant={'h6'}>
-          {'Account Management'}
-        </Typography>
-        <Button
-          sx={{ my: '2px' }}
-          variant={'outlined'}
-          onClick={() => navigate('/profile')} 
-        >
-          {'Profile Page'}
-        </Button>
-      </Box>
+      <AccountManagement />
       <Divider variant="middle" />
-      <Box sx={{ my: 3, mx: 2 }}>
-        <Typography sx={{ marginBottom: '2rem' }} variant={'h6'}>
-        {'Find Match'}
-        </Typography>
-
-        <FormControl fullWidth sx={{ marginBottom: '2rem' }}>
-          <InputLabel>Select Difficulty</InputLabel>
-          <Select
-            value={difficulty}
-            label="Select Difficulty"
-            onChange={(e) => setDifficulty(e.target.value)}
-          >
-            <MenuItem value="Easy">Easy</MenuItem>
-            <MenuItem value="Medium">Medium</MenuItem>
-            <MenuItem value="Hard">Hard</MenuItem>
-          </Select>
-        </FormControl>
-
-        <Button
-          sx={{ my: '2px' }}
-          variant={'outlined'}
-          onClick={() => handleFindMatch(difficulty)}
-        >
-          {'Find Match'}
-        </Button>
-      </Box>
+      <FindMatch />
     </Box>
   )
 }
