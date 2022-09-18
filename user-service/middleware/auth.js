0ac59aa19c26dca8315d62f5b2e35feb5d018989
Lau Siaw Sam <lausiawsam98@gmail.com>
2022-09-18 18:41:12 +0800

    Protect user-service routes with JWT auth checks

---
+++ user-service/middleware/auth.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken'
+import { checkJWTExists } from '../model/redis_repository.js'
+
+const authenticateToken = async (req, res, next) => {
+  const authHeader = req.headers['authorization']
+  const token = authHeader?.split(' ')?.[1]
+  if (!token) {
+    return res.sendStatus(401)
+  }
+
+  // Also check blacklist
+  if (await checkJWTExists(token)) {
+    return res.sendStatus(403)
+  }
+
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, _) => {
+    if (err) {
+      return res.sendStatus(403)
+    } else {
+      req.token = token
+      next()
+    }
+  })
+}
+
+export default authenticateToken
