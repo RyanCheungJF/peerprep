b8cbe266fafa35b777dfcbb24236c938819f5926
Tay Jun Yang <danny_tayjunyang@hotmail.com>
2022-09-19 21:40:54 +0800

    Remove extra indentations in code

--- frontend/src/components/ChangePasswordDialog.js
+++ frontend/src/components/ChangePasswordDialog.js
@@ -10,101 +10,101 @@ import {
   } from '@mui/material'
 import { changeUserPassword } from '../api/userService'
   
-  const ChangePasswordDialog = ({ isDialogOpen, handleCloseDialog}) => {
-    const [newPassword, setNewPassword] = useState('')
-    const [newPasswordTextFieldChanged, setNewPasswordTextFieldChanged] = useState(false)
+const ChangePasswordDialog = ({ isDialogOpen, handleCloseDialog}) => {
+  const [newPassword, setNewPassword] = useState('')
+  const [newPasswordTextFieldChanged, setNewPasswordTextFieldChanged] = useState(false)
 
-    const handleCancel = () => {
+  const handleCancel = () => {
+    setNewPassword('');
+    setNewPasswordTextFieldChanged(false);
+    handleCloseDialog();
+  }
+
+  // Hardcoded value for now, for those doing cookies/ jwt/ context
+  // need to read from there.
+  // To test, check atlas to see if there are changes to the hash.
+  const uuid = '631ad96284e0659e96e66c89'
+
+  const handleChangePassword = async () => {
+    if (!newPassword) {
+      console.log('Password cannot be empty!')  
+      setNewPassword('');
+      setNewPasswordTextFieldChanged(true);
+      return
+    }
+
+    try {
+      const res = await changeUserPassword(uuid, newPassword)
+      console.log('success ', res)
       setNewPassword('');
       setNewPasswordTextFieldChanged(false);
-      handleCloseDialog();
+      handleCloseDialog()
+    } catch (err) {
+      console.log('error: ', err)
     }
-
-    // Hardcoded value for now, for those doing cookies/ jwt/ context
-    // need to read from there.
-    // To test, check atlas to see if there are changes to the hash.
-    const uuid = '631ad96284e0659e96e66c89'
-
-    const handleChangePassword = async () => {
-      if (!newPassword) {
-        console.log('Password cannot be empty!')  
-        setNewPassword('');
-        setNewPasswordTextFieldChanged(true);
-        return
-      }
-
-      try {
-        const res = await changeUserPassword(uuid, newPassword)
-        console.log('success ', res)
-        setNewPassword('');
-        setNewPasswordTextFieldChanged(false);
-        handleCloseDialog()
-      } catch (err) {
-        console.log('error: ', err)
-      }
-    }
-
-    return (
-      <Dialog fullWidth={true} maxWidth="sm" open={isDialogOpen}>
-        <DialogTitle>Change Password</DialogTitle>
-        <DialogContent dividers>
-          <DialogContentText>
-            Please enter your new password below and submit.
-          </DialogContentText>
-          {(!newPassword && !newPasswordTextFieldChanged) && (
-            <div>
-                <TextField
-                  autoFocus
-                  fullWidth
-                  margin="normal"
-                  label="New Password"
-                  type="password"
-                  variant="standard"
-                  value={newPassword}
-                  onChange={(e) => {
-                    setNewPasswordTextFieldChanged(true);
-                    setNewPassword(e.target.value)
-                  }}
-                />
-            </div>
-          )}
-          {(newPassword && newPasswordTextFieldChanged) && (
-            <div>
-                <TextField
-                  autoFocus
-                  fullWidth
-                  margin="normal"
-                  label="New Password"
-                  type="password"
-                  variant="standard"
-                  value={newPassword}
-                  onChange={(e) => setNewPassword(e.target.value)}
-                />
-            </div>
-          )}
-          {(!newPassword && newPasswordTextFieldChanged) && (
-            <div>
-                <TextField
-                  autoFocus
-                  error
-                  fullWidth
-                  margin="normal"
-                  label="New Password"
-                  helperText="New password cannot be empty"
-                  variant="standard"
-                  value={newPassword}
-                  onChange={(e) => setNewPassword(e.target.value)}
-                />
-            </div>
-          )}
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCancel}>Cancel</Button>
-          <Button onClick={handleChangePassword}>Submit</Button>
-        </DialogActions>
-      </Dialog>
-    )
   }
-  
-  export default ChangePasswordDialog
+
+  return (
+    <Dialog fullWidth={true} maxWidth="sm" open={isDialogOpen}>
+      <DialogTitle>Change Password</DialogTitle>
+      <DialogContent dividers>
+        <DialogContentText>
+          Please enter your new password below and submit.
+        </DialogContentText>
+        {(!newPassword && !newPasswordTextFieldChanged) && (
+          <div>
+              <TextField
+                autoFocus
+                fullWidth
+                margin="normal"
+                label="New Password"
+                type="password"
+                variant="standard"
+                value={newPassword}
+                onChange={(e) => {
+                  setNewPasswordTextFieldChanged(true);
+                  setNewPassword(e.target.value)
+                }}
+              />
+          </div>
+        )}
+        {(newPassword && newPasswordTextFieldChanged) && (
+          <div>
+              <TextField
+                autoFocus
+                fullWidth
+                margin="normal"
+                label="New Password"
+                type="password"
+                variant="standard"
+                value={newPassword}
+                onChange={(e) => setNewPassword(e.target.value)}
+              />
+          </div>
+        )}
+        {(!newPassword && newPasswordTextFieldChanged) && (
+          <div>
+              <TextField
+                autoFocus
+                error
+                fullWidth
+                margin="normal"
+                label="New Password"
+                helperText="New password cannot be empty"
+                variant="standard"
+                value={newPassword}
+                onChange={(e) => setNewPassword(e.target.value)}
+              />
+          </div>
+        )}
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleCancel}>Cancel</Button>
+        <Button onClick={handleChangePassword}>Submit</Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
+
+export default ChangePasswordDialog
   
--- frontend/src/components/DeleteAccountDialog.js
+++ frontend/src/components/DeleteAccountDialog.js
@@ -10,35 +10,34 @@ import {
 import { deleteUser } from '../api/userService'
 import { baseUrl } from '../utils/routeConstants'
   
-  const DeleteAccountDialog = ({ isDialogOpen, handleNo }) => {
-    const navigate = useNavigate()
+const DeleteAccountDialog = ({ isDialogOpen, handleNo }) => {
+  const navigate = useNavigate()
 
-    // Hardcoded for now also. Will read this from context next time.
-    const username = 'samtest10'
-    const handleDeleteAccount = async () => {
-      try {
-        await deleteUser(username)
-        navigate(baseUrl)
-      } catch (err) {
-        console.log(`Failed to delete user account: ${err}`)
-      }
+  // Hardcoded for now also. Will read this from context next time.
+  const username = 'samtest10'
+  const handleDeleteAccount = async () => {
+    try {
+      await deleteUser(username)
+      navigate(baseUrl)
+    } catch (err) {
+      console.log(`Failed to delete user account: ${err}`)
     }
-
-    return (
-      <Dialog open={isDialogOpen}>
-        <DialogTitle>Delete Account</DialogTitle>
-        <DialogContent dividers>
-          <DialogContentText>
-            Are you sure you want to delete your account?
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleNo}>No</Button>
-          <Button onClick={handleDeleteAccount}>Yes</Button>
-        </DialogActions>
-      </Dialog>
-    )
   }
-  
-  export default DeleteAccountDialog
-  
+
+  return (
+    <Dialog open={isDialogOpen}>
+      <DialogTitle>Delete Account</DialogTitle>
+      <DialogContent dividers>
+        <DialogContentText>
+          Are you sure you want to delete your account?
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleNo}>No</Button>
+        <Button onClick={handleDeleteAccount}>Yes</Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
+
+export default DeleteAccountDialog
