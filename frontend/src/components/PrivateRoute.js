94edca8a8aa5527cab157ff8cd9932adaf9c7a7e
Lau Siaw Sam <lausiawsam98@gmail.com>
2022-09-21 17:49:06 +0800

    Fetch user info everytime a private route is mounted

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -1,19 +1,76 @@
+import { useState, useEffect } from 'react'
 import { Navigate, useLocation } from 'react-router-dom'
-import { isUserLoggedIn } from '../api/userService'
+import Cookies from 'js-cookie'
+import CircularProgress from '@mui/material/CircularProgress'
+import Box from '@mui/material/Box'
+import { isUserLoggedIn, getUser } from '../api/userService'
 import { loginUrl } from '../utils/routeConstants'
-import { AUTH_REDIRECT } from '../utils/constants'
+import { AUTH_REDIRECT, COOKIES_AUTH_TOKEN } from '../utils/constants'
+
+// enum for the different view states in an FSM-pattern
+const PrivateRouteViewState = {
+  loading: 'LOADING',
+  authed: 'AUTHED',
+  unauthed: 'UNAUTHED',
+}
 
 const PrivateRoute = ({ children }) => {
+  const [viewState, setViewState] = useState(PrivateRouteViewState.loading)
   const location = useLocation()
 
-  if (!isUserLoggedIn()) {
+  useEffect(() => {
+    setViewState(PrivateRouteViewState.loading)
+
+    const authCheck = async () => {
+      if (!isUserLoggedIn()) {
+        setViewState(PrivateRouteViewState.unauthed)
+        return
+      }
+
+      try {
+        const response = await getUser()
+        setViewState(PrivateRouteViewState.authed)
+      } catch (err) {
+        // JWT possibly expired or invalid
+        console.error('Error initializaing app', err)
+        setViewState(PrivateRouteViewState.unauthed)
+      }
+    }
+
+    // set a delay so that the loading state lasts at least 300ms.
+    // Otherwise, the loading spinner might flash for a very short time and cause flicker
+    setTimeout(authCheck, 300)
+  }, [location.pathname])
+
+  const _renderLoading = () => {
+    return (
+      <Box sx={{ display: 'flex', justifyContent: 'center', paddingTop: '5%' }}>
+        <CircularProgress />
+      </Box>
+    )
+  }
+
+  const _renderRedirect = () => {
     // Save the url of the current page before redirecting the user to log in.
     // Allows us to bring the user back to the page he/she was trying to access after logging in.
     window.localStorage.setItem(AUTH_REDIRECT, location.pathname)
+    Cookies.remove(COOKIES_AUTH_TOKEN)
     return <Navigate to={loginUrl} replace={true} />
   }
 
-  return children
+  switch (viewState) {
+    case PrivateRouteViewState.loading:
+      return _renderLoading()
+
+    case PrivateRouteViewState.unauthed:
+      return _renderRedirect()
+
+    case PrivateRouteViewState.authed:
+      return children
+
+    default:
+      return null
+  }
 }
 
 export default PrivateRoute
