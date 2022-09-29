8b93a997e1109b671d4427ae2bbf36bcd9934020
Ryan Cheung <ryan9cheung@gmail.com>
2022-09-29 21:37:43 +0800

    Add guard clause check

--- question-service/controller/question-controller.js
+++ question-service/controller/question-controller.js
@@ -4,8 +4,18 @@ const selectRandomQuestion = (questions) => {
   return questions[Math.floor(Math.random() * questions.length)]
 }
 
+const checkParameters = (param, keywords) => {
+  return keywords.includes(param)
+}
+
 export const getQuestion = async (req, res) => {
   const difficulty = req.query.difficulty
+
+  if (!checkParameters(difficulty, ['easy', 'medium', 'hard'])) {
+    return res
+      .status(500)
+      .json({ message: 'Invalid query parameter supplied.' })
+  }
 
   const questions = await _findByDifficulty(difficulty)
   if (!questions || questions.length == 0) {
