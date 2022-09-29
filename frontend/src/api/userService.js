9b8fb830d1c00f46f47af49921b56074f768c531
Lau Siaw Sam <lausiawsam98@gmail.com>
2022-09-29 10:29:58 +0800

    Change JWT cookies expiry to 15mins

--- frontend/src/api/userService.js
+++ frontend/src/api/userService.js
@@ -5,11 +5,8 @@ import {
   URL_USER_LOGIN_SVC,
   URL_USER_LOGOUT_SVC,
 } from '../utils/configs'
-import {
-  STATUS_CODE_SUCCESS,
-  COOKIES_AUTH_TOKEN,
-  JWT_EXPIRY,
-} from '../utils/constants'
+import { STATUS_CODE_SUCCESS, COOKIES_AUTH_TOKEN } from '../utils/constants'
+import { getJWTExpiry } from '../utils/main'
 
 export const getUser = async () => {
   return axios.get(URL_USER_SVC, {
@@ -20,7 +17,7 @@ export const getUser = async () => {
 export const loginUser = async (username, password) => {
   const res = await axios.post(URL_USER_LOGIN_SVC, { username, password })
   if (res?.status === STATUS_CODE_SUCCESS) {
-    Cookies.set(COOKIES_AUTH_TOKEN, res.data.token, { expires: JWT_EXPIRY })
+    Cookies.set(COOKIES_AUTH_TOKEN, res.data.token, { expires: getJWTExpiry() })
   }
   return res
 }
