307ec24d4949aa6dbb00b4ba6d7ecd339a2565a8
Lau Siaw Sam <lausiawsam98@gmail.com>
2022-10-07 17:19:35 +0800

    Clean up code

--- collaboration-service/controller/shared-code-controller.js
+++ collaboration-service/controller/shared-code-controller.js
@@ -1,8 +1,6 @@
 import { saveSharedCode } from '../model/redis-repository.js'
 
-export const sharedCodeHandler = (socket) => (code, room) => {
-  console.log(`Pushing up code for room ${room}: ${code}`)
-
-  socket.to(room).emit('pull-code', code)
-  saveSharedCode(code, room)
+export const sharedCodeHandler = (socket) => (code, roomId) => {
+  socket.to(roomId).emit('pull-code', code)
+  saveSharedCode(code, roomId)
 }
