beffa0d33fdbd202fe507f69ba185fb3692a811b
Tay Jun Yang <danny_tayjunyang@hotmail.com>
2022-10-14 13:34:54 +0800

    Implement snackbar alert for change password

--- frontend/src/components/ChangePasswordDialog.js
+++ frontend/src/components/ChangePasswordDialog.js
@@ -10,12 +10,22 @@ import {
 } from '@mui/material'
 import { UserContext } from '../contexts/UserContext'
 import { changeUserPassword } from '../api/userService'
+import SnackbarAlert from './SnackbarAlert'
 
 const ChangePasswordDialog = ({ dialogOpen, handleCloseDialog }) => {
   const [newPassword, setNewPassword] = useState('')
   const [newPasswordTextFieldChanged, setNewPasswordTextFieldChanged] =
     useState(false)
+
   const user = useContext(UserContext)
+
+  // Change Password Successful Alert
+  const [
+    changePasswordSuccessfulAlertOpen,
+    setChangePasswordSuccessfulAlertOpen,
+  ] = useState(false)
+  const handleChangePasswordSuccessfulOpenAlert = () =>
+    setChangePasswordSuccessfulAlertOpen(true)
 
   const handleCancel = () => {
     setNewPassword('')
@@ -37,64 +47,85 @@ const ChangePasswordDialog = ({ dialogOpen, handleCloseDialog }) => {
       setNewPassword('')
       setNewPasswordTextFieldChanged(false)
       handleCloseDialog()
+      handleChangePasswordSuccessfulOpenAlert()
     } catch (err) {
       console.log('error: ', err)
     }
   }
 
+  const renderChangePasswordSuccessfulAlert = () => {
+    if (!changePasswordSuccessfulAlertOpen) {
+      return null
+    }
+
+    return (
+      <>
+        <SnackbarAlert
+          alertOpen={changePasswordSuccessfulAlertOpen}
+          setAlertOpen={setChangePasswordSuccessfulAlertOpen}
+          severity="success"
+          alertMsg="Your password has been changed successfully!"
+        />
+      </>
+    )
+  }
+
   return (
-    <Dialog fullWidth={true} maxWidth="sm" open={dialogOpen}>
-      <DialogTitle>Change Password</DialogTitle>
-      <DialogContent dividers>
-        <DialogContentText>
-          Please enter your new password below and submit.
-        </DialogContentText>
-        {!newPassword && !newPasswordTextFieldChanged && (
-          <TextField
-            autoFocus
-            fullWidth
-            margin="normal"
-            label="New Password"
-            type="password"
-            variant="standard"
-            value={newPassword}
-            onChange={(e) => {
-              setNewPasswordTextFieldChanged(true)
-              setNewPassword(e.target.value)
-            }}
-          />
-        )}
-        {newPassword && newPasswordTextFieldChanged && (
-          <TextField
-            autoFocus
-            fullWidth
-            margin="normal"
-            label="New Password"
-            type="password"
-            variant="standard"
-            value={newPassword}
-            onChange={(e) => setNewPassword(e.target.value)}
-          />
-        )}
-        {!newPassword && newPasswordTextFieldChanged && (
-          <TextField
-            autoFocus
-            error
-            fullWidth
-            margin="normal"
-            label="New Password"
-            helperText="New password cannot be empty"
-            variant="standard"
-            value={newPassword}
-            onChange={(e) => setNewPassword(e.target.value)}
-          />
-        )}
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleCancel}>Cancel</Button>
-        <Button onClick={handleChangePassword}>Submit</Button>
-      </DialogActions>
-    </Dialog>
+    <>
+      <Dialog fullWidth={true} maxWidth="sm" open={dialogOpen}>
+        <DialogTitle>Change Password</DialogTitle>
+        <DialogContent dividers>
+          <DialogContentText>
+            Please enter your new password below and submit.
+          </DialogContentText>
+          {!newPassword && !newPasswordTextFieldChanged && (
+            <TextField
+              autoFocus
+              fullWidth
+              margin="normal"
+              label="New Password"
+              type="password"
+              variant="standard"
+              value={newPassword}
+              onChange={(e) => {
+                setNewPasswordTextFieldChanged(true)
+                setNewPassword(e.target.value)
+              }}
+            />
+          )}
+          {newPassword && newPasswordTextFieldChanged && (
+            <TextField
+              autoFocus
+              fullWidth
+              margin="normal"
+              label="New Password"
+              type="password"
+              variant="standard"
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
+            />
+          )}
+          {!newPassword && newPasswordTextFieldChanged && (
+            <TextField
+              autoFocus
+              error
+              fullWidth
+              margin="normal"
+              label="New Password"
+              helperText="New password cannot be empty"
+              variant="standard"
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
+            />
+          )}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCancel}>Cancel</Button>
+          <Button onClick={handleChangePassword}>Submit</Button>
+        </DialogActions>
+      </Dialog>
+      {renderChangePasswordSuccessfulAlert()}
+    </>
   )
 }
 
