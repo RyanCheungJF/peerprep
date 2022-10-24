6bf78ed459df521fab4f52709091f484a93e3636
Tay Jun Yang <danny_tayjunyang@hotmail.com>
2022-10-25 01:49:33 +0800

    Update CodeEditor component to support overflow and scrolling

--- frontend/src/components/CodeEditor.js
+++ frontend/src/components/CodeEditor.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Editor from 'react-simple-code-editor'
-import { Box, Paper } from '@mui/material'
+import { Box, Divider, Paper } from '@mui/material'
 import { highlight, languages } from 'prismjs/components/prism-core'
 import 'prismjs/components/prism-clike'
 import 'prismjs/components/prism-javascript'
@@ -26,22 +26,28 @@ const CodeEditor = ({ room }) => {
 
   return (
     <Paper sx={{ height: '100%', width: '100%' }} elevation={5}>
-      <Box className="code-editor-wrapper">
-        <p className="code-editor-title">Code Editor</p>
-        <Editor
-          value={code}
-          onValueChange={handleCodeChange}
-          highlight={(code) => highlight(code, languages.js)}
-          padding={10}
-          style={{
-            width: '100%',
-            height: '100%',
-            fontFamily: '"Fira code", "Fira Mono", monospace',
-            fontSize: '1.1rem',
-            backgroundColor: '#F5F5F5',
-            border: '1px solid rgba(0, 0, 0, 0.15)',
-          }}
-        />
+      <Box className="code-editor-div">
+        <Box className="code-editor-title-wrapper">
+          <p className="code-editor-title">Code Editor</p>
+          <Divider />
+        </Box>
+        <Box className="code-editor-editor-wrapper">
+          <Editor
+            value={code}
+            onValueChange={handleCodeChange}
+            highlight={(code) => highlight(code, languages.js)}
+            padding={20}
+            style={{
+              width: '100%',
+              minHeight: '100%',
+              position: 'absolute',
+              overflow: 'auto',
+              fontFamily: '"Fira code", "Fira Mono", monospace',
+              fontSize: '1.1rem',
+              backgroundColor: '#F5F5F5',
+            }}
+          />
+        </Box>
       </Box>
     </Paper>
   )
