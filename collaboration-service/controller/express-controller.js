e6db81a500f0ea01b71bfd70bbd2dc5d55c20773
Lau Siaw Sam <lausiawsam98@gmail.com>
2022-10-26 18:23:36 +0800

    Bugfix: clear collab-svc cache on user deletion

--- collaboration-service/controller/express-controller.js
+++ collaboration-service/controller/express-controller.js
@@ -1,4 +1,5 @@
 import { ormDeleteRoom as _deleteRoom } from '../model/collaboration-room-orm.js'
+import { deleteRoom as _redisDeleteRoom } from '../model/redis-repository.js'
 import { COLLAB_ROOM_PREFIX } from '../constants.js'
 
 export const deleteRoom = async (req, res) => {
@@ -7,12 +8,20 @@ export const deleteRoom = async (req, res) => {
     return res.status(400).json({ message: 'Room ID not provided.' })
   }
 
-  const deletionResult = await _deleteRoom(`${COLLAB_ROOM_PREFIX}${roomId}`)
+  const collabRoomId = `${COLLAB_ROOM_PREFIX}${roomId}`
+  const [dbResult, redisResult] = await Promise.allSettled([
+    _deleteRoom(collabRoomId),
+    _redisDeleteRoom(collabRoomId), //redis operation doesn't fail or throw erors
+  ])
 
-  if (deletionResult.err) {
+  if (
+    dbResult.status === 'rejected' ||
+    dbResult.value.err ||
+    redisResult.status === 'rejected'
+  ) {
     return res.status(500).json({ message: 'Error deleting room.' })
   }
-  if (deletionResult.deletedCount === 0) {
+  if (dbResult.value.deletedCount === 0) {
     return res.status(404).json({ message: `Room ${roomId} doesn't exist.` })
   }
 
