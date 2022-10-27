9b836bf8bd12de36337f8ff85b9f2a42baddb91c
Lau Siaw Sam <lausiawsam98@gmail.com>
2022-10-27 21:09:07 +0800

    fix typo in matching-service log

--- user-service/services/matching-service.js
+++ user-service/services/matching-service.js
@@ -6,7 +6,7 @@ export const deleteMatchRoom = async (username) => {
     return axios.delete(`${URL_MATCHING_SVC}user/${username}/room`)
   } catch (error) {
     console.error(
-      `Failed to delete matching-svc room for user ${userId}. Check matching-svc logs for more details.`
+      `Failed to delete matching-svc room for user ${username}. Check matching-svc logs for more details.`
     )
   }
 }
